8b18d1f4e8d7e15a850e0ee7dd237f32a2f100a1
CesarOrdaz7 <106408947+CesarOrdaz7@users.noreply.github.com>
2022-06-04 20:52:07 -0600

    Add files via upload

---
+++ Ejercicio Retador 1.py
@@ -0,0 +1,16 @@
+Estado = "Sinaloa"
+print(Estado)
+
+Poblacion_mujeres = 1532128
+Poblacion_hombres = 1494815
+Poblacion_total = str(Poblacion_mujeres + Poblacion_hombres)
+print("Tiene " + Poblacion_total + " habitantes")
+
+Porcentaje_Poblacion_Culiacan = 35.15
+Porcentaje_Poblacion_Mazatlan = 16.57
+Porcentaje_Total_Cul_Maz = str(Porcentaje_Poblacion_Culiacan + Porcentaje_Poblacion_Mazatlan)
+print("El " + Porcentaje_Total_Cul_Maz + "% corresponde a la poblacion de Culiacán y Mazatlán")
+
+Tipos_Clima = "El clima es cálido, subhúmedo, seco y semiseco"
+print(Tipos_Clima)
+
